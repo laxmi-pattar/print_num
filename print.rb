1b8b75a1d63bb065b21d6f5cc645aed7a01ce8d9
Laxmi Pattar <laxmipattar710@gmail.com>
2019-07-18 18:40:20 +0530

    print_num

---
+++ print.rb
@@ -0,0 +1,10 @@
+def print_num
+  i = 0
+  for i in 0..100
+    if i % 2 == 0  
+      puts "#{i}"
+    end
+  end
+end
+
+#print_num
---
+++ test/test_print.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require_relative '../print'
+
+class TestArray < Minitest::Test
+  def test_print
+    assert_equal = print_num
+  end
+end
